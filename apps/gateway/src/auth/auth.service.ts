2bdcb531e2b3de301de466a847224b1537376e5b
AmirBazanov <flash5651311@gmail.com>
2025-04-23 17:44:57 +0300

    Added protogenerator and script to copy proto to dist (configs minor fixes)

--- apps/gateway/src/auth/auth.service.ts
+++ apps/gateway/src/auth/auth.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import {Inject, Injectable, OnModuleInit} from '@nestjs/common';
+import {AuthRegisterDto} from "../../../../libs/dto/auth.dto";
+import {ClientGrpc} from "@nestjs/microservices";
+import {AuthServiceClient} from "../../../../proto/generated/auth";
+
+
 
 @Injectable()
-export class AuthService {}
+export class AuthService implements OnModuleInit{
+    private authService: AuthServiceClient
+    constructor(@Inject('AUTH_PACKAGE') private client: ClientGrpc) {}
+    onModuleInit(){
+        this.authService = this.client.getService<AuthServiceClient>("AuthService")
+    }
+    async register(dto: AuthRegisterDto){
+        this.authService.register(dto).subscribe((resp)=>{return resp})
+    }
+}
