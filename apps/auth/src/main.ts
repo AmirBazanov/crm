d9c339e35f63947c8cf39ab28023011140f6d718
AmirBazanov <flash5651311@gmail.com>
2025-04-23 17:47:33 +0300

    reconfigured entrypoints

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import {ValidationPipe} from "@nestjs/common";
+import {MicroserviceOptions, Transport} from "@nestjs/microservices";
+import {join} from "path";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+      AuthModule,
+      {
+        transport: Transport.GRPC,
+        options: {
+          protoPath: join(__dirname, '../../proto/auth.proto'),
+          package: "auth",
+            url: "0.0.0.0:5001"
+        },
+      },
+  );
+  app.useGlobalPipes(new ValidationPipe())
+  await app.listen();
 }
 bootstrap();
--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
-import {ValidationPipe} from "@nestjs/common";
+
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
-  await app.listen(process.env.port ?? 3000);
-  app.useGlobalPipes(new ValidationPipe())
+  await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().then(() => null);
