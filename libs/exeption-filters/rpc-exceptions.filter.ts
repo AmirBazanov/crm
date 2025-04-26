13baf5b0bd4fed39da26e431ce0ca09d49eadb6f
AmirBazanov <flash5651311@gmail.com>
2025-04-26 11:30:19 +0300

    feat(lib): add exceptions filters

---
+++ libs/exeption-filters/rpc-exceptions.filter.ts
@@ -0,0 +1,16 @@
+import {
+    Catch,
+    ArgumentsHost,
+} from '@nestjs/common';
+import { BaseRpcExceptionFilter, RpcException } from '@nestjs/microservices';
+
+@Catch()
+export class GrpcExceptionFilter extends BaseRpcExceptionFilter {
+    catch(exception: unknown, host: ArgumentsHost) {
+        if (exception instanceof RpcException) {
+            return super.catch(exception, host);
+        }
+
+        return super.catch(new RpcException('Unexpected error'), host);
+    }
+}
