9f52f8b6b1e0385aed26830879a4ebf77e06ade0
AmirBazanov <flash5651311@gmail.com>
2025-05-14 15:47:53 +0300

    feat(go-logger): add logger config

--- services/users/cmd/users/main.go
+++ services/users/cmd/users/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	gologger "crm/libs/go-logger"
 	"crm/services/users/internal/config"
 	"fmt"
+	"log/slog"
 )
 
 func main() {
 	cfg := config.MustLoad()
 	fmt.Println(cfg)
+	logger := setupLogger(cfg)
+	logger.Error("Logger initialized")
+}
+
+func setupLogger(config *config.Config) *slog.Logger {
+	logCfg := config.Logger
+	gologger.InitLogger(logCfg.Service, logCfg.LogLevel, logCfg.LogFile)
+	return gologger.GetLogger()
 }
