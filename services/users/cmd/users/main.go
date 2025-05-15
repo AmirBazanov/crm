1dae6228b28a0c3ac1df996da0a5d745a75d0871
AmirBazanov <flash5651311@gmail.com>
2025-05-15 14:52:00 +0300

    feat(users/cmd): Initialize grpc server and add graceful stop (shutdown) by signals

--- services/users/cmd/users/main.go
+++ services/users/cmd/users/main.go
@@ -2,20 +2,33 @@ package main
 
 import (
 	gologger "crm/libs/go-logger"
+	"crm/services/users/internal/app"
 	"crm/services/users/internal/config"
-	"fmt"
 	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
 	logger := setupLogger(cfg)
-	logger.Error("Logger initialized")
+	logger.Info("Logger initialized")
+	application := app.New(logger, cfg.GRPC.Port, cfg.DbUrl)
+	go application.GRPCSrv.MustRun()
+	GrpcStop(application, logger)
 }
 
 func setupLogger(config *config.Config) *slog.Logger {
 	logCfg := config.Logger
 	gologger.InitLogger(logCfg.Service, logCfg.LogLevel, logCfg.LogFile)
 	return gologger.GetLogger()
+}
+
+func GrpcStop(app *app.App, logger *slog.Logger) {
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+	sign := <-stop
+	app.GRPCSrv.Stop()
+	logger.Info("Application stopped", "signal", sign)
 }
