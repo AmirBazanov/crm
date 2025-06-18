98a9cab37e316ff46c9da6a14b6e282297590be3
AmirBazanov <flash5651311@gmail.com>
2025-06-18 13:27:47 +0300

    feat(user): add interfaces and add Get(Email,Username), Update, Delete, Search methods

--- services/users/internal/services/users/user.go
+++ services/users/internal/services/users/user.go
@@ -24,6 +24,14 @@ type UserGetBy interface {
 	UserByEmail(ctx context.Context, email string) (users *databaseusers.Users, err error)
 }
 
+type UserUpdate interface {
+	UserUpdate(ctx context.Context, users *databaseusers.Users) (user *databaseusers.Users, err error)
+}
+
+type UserDelete interface {
+	UserDelete(ctx context.Context, id string) (err error)
+}
+
 type SearchByCredentials interface {
 	SearchUserByCredentials(ctx context.Context, usersCred *databaseusers.Users) (users *databaseusers.Users, err error)
 }
@@ -37,7 +45,7 @@ func New(logger *slog.Logger, db *postgresgorm.Storage) *User {
 
 func (u *User) Create(ctx context.Context, users *databaseusers.Users) (id string, err error) {
 	const op = "User.Create"
-	u.logger.Info("creating user", op)
+	u.logger.Info("creating user", op, users.ID)
 	resId, resErr := u.db.UserCreate(ctx, users)
 	if errors.Is(resErr, constants.ErrUserAlreadyExists) {
 		u.logger.Warn(op, constants.ErrUserAlreadyExists)
@@ -63,4 +71,80 @@ func (u *User) GetById(ctx context.Context, id string) (users *databaseusers.Use
 		return nil, resErr
 	}
 	return resUser, nil
+}
+
+func (u *User) GetByUsername(ctx context.Context, username string) (users *databaseusers.Users, err error) {
+	const op = "User.GetByUsername"
+	u.logger.Info("getting user by username", op, username)
+	resUser, resErr := u.db.UserByUsername(ctx, username)
+	if errors.Is(resErr, constants.ErrUserNotFound) {
+		u.logger.Warn(op, constants.ErrUserNotFound)
+		return nil, constants.ErrUserNotFound
+	}
+	if resErr != nil {
+		u.logger.Error(op, resErr)
+		return nil, resErr
+	}
+	return resUser, nil
+}
+
+func (u *User) GetByEmail(ctx context.Context, email string) (users *databaseusers.Users, err error) {
+	const op = "User.GetByEmail"
+	u.logger.Info("getting user by email", op, email)
+	resUser, resErr := u.db.UserByEmail(ctx, email)
+	if errors.Is(resErr, constants.ErrUserNotFound) {
+		u.logger.Warn(op, constants.ErrUserNotFound)
+		return nil, constants.ErrUserNotFound
+	}
+	if resErr != nil {
+		u.logger.Error(op, resErr)
+		return nil, resErr
+	}
+	return resUser, nil
+}
+
+func (u *User) Update(ctx context.Context, users *databaseusers.Users) (user *databaseusers.Users, err error) {
+	const op = "User.Update"
+	u.logger.Info("updating user", op, users)
+	resUser, resErr := u.db.UserUpdate(ctx, users)
+	if errors.Is(resErr, constants.ErrUserNotFound) {
+		u.logger.Warn(op, constants.ErrUserNotFound)
+		return nil, constants.ErrUserNotFound
+	}
+	if resErr != nil {
+		u.logger.Error(op, resErr)
+		return nil, resErr
+	}
+	return resUser, nil
+}
+
+func (u *User) Delete(ctx context.Context, id string) (err error) {
+	const op = "User.Delete"
+	u.logger.Info("deleting user", op, id)
+	resErr := u.db.UserDelete(ctx, id)
+	if errors.Is(resErr, constants.ErrUserNotFound) {
+		u.logger.Warn(op, constants.ErrUserNotFound)
+		return constants.ErrUserNotFound
+
+	}
+	if resErr != nil {
+		u.logger.Error(op, resErr)
+		return resErr
+	}
+	return nil
+}
+
+func (u *User) Search(ctx context.Context, usersCred *databaseusers.Users) (users *databaseusers.Users, err error) {
+	const op = "User.Search"
+	u.logger.Info("searching users", op, usersCred)
+	resU, resErr := u.db.SearchUserByCredentials(ctx, usersCred)
+	if errors.Is(resErr, constants.ErrUserNotFound) {
+		u.logger.Warn(op, constants.ErrUserNotFound)
+		return nil, constants.ErrUserNotFound
+	}
+	if resErr != nil {
+		u.logger.Error(op, resErr)
+		return nil, resErr
+	}
+	return resU, nil
 }
