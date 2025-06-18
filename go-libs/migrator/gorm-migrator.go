6058e6dff5748ede84fdfff4681454f3b829d5d1
AmirBazanov <flash5651311@gmail.com>
2025-06-18 13:22:19 +0300

    feat(db-migrator): rework migrator to logg and run correctly

--- go-libs/migrator/gorm-migrator.go
+++ go-libs/migrator/gorm-migrator.go
@@ -1,10 +1,20 @@
 package migratorgorm
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log/slog"
+)
 
-func Migrate(db *gorm.DB, schemas ...interface{}) {
-	err := db.AutoMigrate(schemas...)
+func Migrate(dbUrl string, logger *slog.Logger, schemas ...interface{}) {
+	db, err := gorm.Open(postgres.Open(dbUrl))
+	if err != nil {
+		panic("failed to connect database")
+	}
+	logger.Info("Migrating database")
+	err = db.AutoMigrate(schemas...)
 	if err != nil {
 		panic(err)
 	}
+	logger.Info("Database migrated successfully")
 }
