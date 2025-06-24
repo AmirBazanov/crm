aadceefd44ef24c5df3dd959db552fe283ae4f79
AmirBazanov <flash5651311@gmail.com>
2025-06-24 14:28:14 +0300

    feat(migrator): fix countries seeding

--- go_libs/migrator/gorm_migrator.go
+++ go_libs/migrator/gorm_migrator.go
@@ -8,23 +8,36 @@ import (
 )
 
 func Migrate(dbUrl string, logger *slog.Logger, schemas ...interface{}) {
-	countries := []*databaseusers.Countries{
-		{Code: "NIL", Name: "UNSPECIFIED"},
-		{Code: "EN", Name: "England"},
-		{Code: "IT", Name: "Italy"},
-		{Code: "FR", Name: "France"},
-		{Code: "DE", Name: "Germany"},
-		{Code: "RU", Name: "RUSSIA"},
-	}
 	db, err := gorm.Open(postgres.Open(dbUrl))
 	if err != nil {
 		panic("failed to connect database")
 	}
+
 	logger.Info("Migrating database")
 	err = db.AutoMigrate(schemas...)
-	err = db.AutoMigrate(countries)
 	if err != nil {
 		panic(err)
 	}
-	logger.Info("Database migrated successfully")
+
+	logger.Info("Seeding countries")
+	var count int64
+	db.Model(&databaseusers.Countries{}).Count(&count)
+
+	if count == 0 {
+		countries := []databaseusers.Countries{
+			{Code: "NIL", Name: "UNSPECIFIED"},
+			{Code: "EN", Name: "England"},
+			{Code: "IT", Name: "Italy"},
+			{Code: "FR", Name: "France"},
+			{Code: "DE", Name: "Germany"},
+			{Code: "RU", Name: "RUSSIA"},
+		}
+		if err := db.Create(&countries).Error; err != nil {
+			logger.Error("Failed to seed countries", slog.String("error", err.Error()))
+		} else {
+			logger.Info("Countries seeded")
+		}
+	} else {
+		logger.Info("Countries already seeded, skipping")
+	}
 }
