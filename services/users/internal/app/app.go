b8db3cbe046cb50fe6096c2ab2debfc548eb9b78
AmirBazanov <flash5651311@gmail.com>
2025-06-25 22:20:53 +0300

    feat(redis): provide highlevel deps for redis in user sevice

--- services/users/internal/app/app.go
+++ services/users/internal/app/app.go
@@ -1,9 +1,12 @@
 package app
 
 import (
+	"context"
 	grpcusers "crm/services/users/internal/app/grpc"
+	"crm/services/users/internal/config"
 	"crm/services/users/internal/services/users"
 	postgresgorm "crm/services/users/internal/storage/postgres_gorm"
+	"crm/services/users/pkg/redis"
 	"log/slog"
 )
 
@@ -11,13 +14,16 @@ type App struct {
 	GRPCSrv *grpcusers.App
 }
 
-func New(logger *slog.Logger, grpcPort int, dbUrl string) *App {
+func New(logger *slog.Logger, grpcPort int, dbUrl string, redisCfg config.RedisConfig, ctx context.Context) *App {
 	storage, err := postgresgorm.New(logger, dbUrl)
-
+	cache, errRed := redis.NewClient(logger, redisCfg.Addr, redisCfg.Password, redisCfg.DB, ctx)
+	if errRed != nil {
+		logger.Error("Cannot connect to redis", errRed)
+	}
 	if err != nil {
 		panic(err)
 	}
-	userService := users.New(logger, storage)
+	userService := users.New(logger, storage, cache)
 	grpcApp := grpcusers.New(logger, grpcPort, userService)
 
 	return &App{
