0c498e62b23475b9a8dbb2c06e73192d5798ce49
AmirBazanov <flash5651311@gmail.com>
2025-07-03 20:44:38 +0300

    feat(redis): rewrite set get to dedicated functions

--- go_libs/interceptors/redis_caching_interceptor.go
+++ go_libs/interceptors/redis_caching_interceptor.go
@@ -11,7 +11,12 @@ import (
 	"strings"
 )
 
+var Cache *redis.Client
+var Logger *slog.Logger
+
 func CacheUnaryInterceptor(cache *redis.Client, logger *slog.Logger) grpc.UnaryServerInterceptor {
+	Cache = cache
+	Logger = logger
 	return func(
 		ctx context.Context,
 		req interface{},
@@ -22,30 +27,52 @@ func CacheUnaryInterceptor(cache *redis.Client, logger *slog.Logger) grpc.UnaryS
 		if !isSafeMethod(info.FullMethod) {
 			return handler(ctx, req)
 		}
-
 		key := generateCacheKey(info.FullMethod, req)
-
-		if cachedStr, err := cache.Get(key, ctx); err == nil && cachedStr != "" {
-			respType := getResponseTypeForMethod(info.FullMethod)
-			if err := json.Unmarshal([]byte(cachedStr), respType); err == nil {
-				logger.Info("cache hit", "method", info.FullMethod)
-				return respType, nil
-			}
+		if data, err := getFromCache(ctx, key, info.FullMethod); err == nil && data != nil {
+			return data, nil
 		}
-
 		resp, err := handler(ctx, req)
 		if err != nil {
 			return resp, err
 		}
 
-		if data, err := json.Marshal(resp); err == nil {
-			_ = cache.Set(key, data, ctx)
-			logger.Info("cache set", "method", info.FullMethod)
-		}
+		_ = setCache(ctx, resp, key, info.FullMethod)
 		return resp, nil
+
 	}
 }
 
+func getFromCache(ctx context.Context, key string, fullMethod string) (data interface{}, err error) {
+	cachedStr, er := Cache.Get(key, ctx)
+	if er == nil && cachedStr != "" {
+		respType := getResponseTypeForMethod(fullMethod)
+		if err := json.Unmarshal([]byte(cachedStr), respType); err == nil {
+			Logger.Info("cache hit", "method", fullMethod)
+			return respType, nil
+		}
+	}
+	return "", er
+}
+
+func setCache(ctx context.Context, data interface{}, key string, fullMethod string) error {
+	data, err := json.Marshal(data)
+	if err == nil {
+		err = Cache.Set(key, data, ctx)
+		if err != nil {
+			Logger.Info("error caching data", "method", fullMethod, "key", key)
+		}
+		Logger.Info("cache set", "method", fullMethod)
+		return nil
+	}
+	return err
+}
+
+// TODO: Add deleting on Update and delete, or add deleting from service itself
+//func deleteCache(ctx context.Context, key string) error {
+//
+//}
+
+// TODO: Make key simple (Id:123, username: 123)
 func generateCacheKey(method string, req interface{}) string {
 	reqBytes, _ := json.Marshal(req)
 	return fmt.Sprintf("grpc_cache:%s:%s", method, string(reqBytes))
@@ -75,6 +102,8 @@ var responseTypeMap = map[string]func() interface{}{
 	"/users.v3.UserService/Search": func() interface{} {
 		return &usersv3.SearchUsersResponse{}
 	},
+	"/users.v3.UserService/UpdateUser": func() interface{} { return &usersv3.UpdateUserResponse{} },
+	"/users.v3.UserService/DeleteUser": func() interface{} { return &usersv3.DeleteUserResponse{} },
 }
 
 func getResponseTypeForMethod(method string) interface{} {
