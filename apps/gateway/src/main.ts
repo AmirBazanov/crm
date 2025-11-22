dd607649858c5af282514b6c1221a50893ecbc8c
AmirBazanov <flash5651311@gmail.com>
2025-11-23 00:39:16 +0300

    feat(gateway): add validation pipe

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
@@ -8,6 +9,10 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+  app.useGlobalPipes(new ValidationPipe({  whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+    transformOptions: { enableImplicitConversion: true },}));
   await app.listen(3000);
 }
 void bootstrap();
