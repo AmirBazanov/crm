d6f150c61f007667af01460d313c31d8c31847b0
AmirBazanov <flash5651311@gmail.com>
2025-11-23 00:37:37 +0300

    feat(gorm): rewrite slog adapter for safer log without sensitivity data

--- go_libs/storage/slogapapter/logger_interface.go
+++ go_libs/storage/slogapapter/logger_interface.go
@@ -5,15 +5,21 @@ import (
 	"fmt"
 	"gorm.io/gorm/logger"
 	"log/slog"
+	"strings"
 	"time"
 )
 
 type SlogAdapter struct {
-	Log *slog.Logger
+	Log   *slog.Logger
+	Level logger.LogLevel
 }
 
 func (l *SlogAdapter) LogMode(logger.LogLevel) logger.Interface {
 	return l
+}
+
+func (l *SlogAdapter) shouldLog(level logger.LogLevel) bool {
+	return level <= l.Level
 }
 
 func (l *SlogAdapter) Info(ctx context.Context, s string, args ...interface{}) {
@@ -29,6 +35,18 @@ func (l *SlogAdapter) Error(ctx context.Context, s string, args ...interface{}) 
 }
 
 func (l *SlogAdapter) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), _ error) {
-	sql, rows := fc()
-	l.Log.InfoContext(ctx, "gorm", slog.String("sql", sql), slog.Int64("rows", rows), slog.Duration("elapsed", time.Since(begin)))
+	sql, row := fc()
+	query := ""
+	if strings.Contains(sql, "INSERT INTO") {
+		query = "INSERT"
+	} else if strings.Contains(sql, "SELECT") {
+		query = "SELECT"
+	} else if strings.Contains(sql, "UPDATE") {
+		query = "UPDATE"
+	} else if strings.Contains(sql, "DELETE FROM") {
+		query = "DELETE"
+	}
+
+	l.Log.InfoContext(ctx, "gorm", slog.String("sql", query), slog.Int64("rows", row), slog.Duration("elapsed", time.Since(begin)))
+
 }
