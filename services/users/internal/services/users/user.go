7f371f7610b88c4240f24b2c212ba9cb3dbc0c28
AmirBazanov <flash5651311@gmail.com>
2025-11-23 00:30:50 +0300

    feat(user-service): add find by nickname method

--- services/users/internal/services/users/user.go
+++ services/users/internal/services/users/user.go
@@ -15,7 +15,7 @@ type User struct {
 
 type Storage interface {
 	UserByID(ctx context.Context, id uint32) (users *databaseusers.Users, err error)
-	UserByUsername(ctx context.Context, username string) (users *databaseusers.Users, err error)
+	UserByNickname(ctx context.Context, username string) (users *databaseusers.Users, err error)
 	UserByEmail(ctx context.Context, email string) (users *databaseusers.Users, err error)
 	UsersGet(ctx context.Context) (users []*databaseusers.Users, err error)
 	UserCreate(ctx context.Context, users *databaseusers.Users) (id uint32, err error)
@@ -33,7 +33,7 @@ func New(logger *slog.Logger, db Storage) *User {
 
 func (u *User) Create(ctx context.Context, users *databaseusers.Users) (id uint32, err error) {
 	const op = "User.Create"
-	u.logger.Info("creating user", op, users.ID)
+	u.logger.Info("creating user", op, users.Nickname)
 	resId, resErr := u.db.UserCreate(ctx, users)
 	if errors.Is(resErr, constants.ErrUserAlreadyExists) {
 		u.logger.Warn(op, constants.ErrUserAlreadyExists)
@@ -61,16 +61,16 @@ func (u *User) GetById(ctx context.Context, id uint32) (users *databaseusers.Use
 	return resUser, nil
 }
 
-func (u *User) GetByUsername(ctx context.Context, username string) (users *databaseusers.Users, err error) {
-	const op = "User.GetByUsername"
-	u.logger.Info("getting user by username", op, username)
-	resUser, resErr := u.db.UserByUsername(ctx, username)
+func (u *User) GetByNickname(ctx context.Context, nickname string) (users *databaseusers.Users, err error) {
+	const op = "User.GetByNickname"
+	u.logger.Info("getting user by nickname", op, nickname)
+	resUser, resErr := u.db.UserByNickname(ctx, nickname)
 	if errors.Is(resErr, constants.ErrUserNotFound) {
-		u.logger.Warn(op, constants.ErrUserNotFound)
+		u.logger.Warn(op, constants.ErrUserNotFound.Error())
 		return nil, constants.ErrUserNotFound
 	}
 	if resErr != nil {
-		u.logger.Error(op, resErr)
+		u.logger.Error(op, resErr.Error())
 		return nil, resErr
 	}
 	return resUser, nil
